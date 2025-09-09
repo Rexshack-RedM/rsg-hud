4257f17c266964f359d9afee0cc98021393faccb
StrixLuca <110634650+StrixLuca@users.noreply.github.com>
2025-09-09 10:41:52 +0200

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -1,40 +1,56 @@
 local RSGCore = exports['rsg-core']:GetCoreObject()
-local ResetStress = false
-lib.locale()
 
-RSGCore.Commands.Add('cash', 'Check Cash Balance', {}, false, function(source, args)
-    local Player = RSGCore.Functions.GetPlayer(source)
-    local cashamount = Player.PlayerData.money.cash
-    if cashamount ~= nil then
-        TriggerClientEvent('hud:client:ShowAccounts', source, 'cash', cashamount)
-    else
-        return
-    end
-end)
 
-RSGCore.Commands.Add('bloodmoney', 'Check Bloodmoney Balance', {}, false, function(source, args)
-    local Player = RSGCore.Functions.GetPlayer(source)
-    local bloodmoneyamount = Player.PlayerData.money.bloodmoney
-    if bloodmoneyamount ~= nil then
-        TriggerClientEvent('hud:client:ShowAccounts', source, 'bloodmoney', bloodmoneyamount)
-    else
-        return
-    end
-end)
-
----------------------------------
--- get outlaw status
----------------------------------
-RSGCore.Functions.CreateCallback('hud:server:getoutlawstatus', function(source, cb)
+RegisterNetEvent('RSGCore:Server:OnPlayerLoaded', function()
     local src = source
     local Player = RSGCore.Functions.GetPlayer(src)
-    if Player ~= nil then
-        MySQL.query('SELECT outlawstatus FROM players WHERE citizenid = ?', {Player.PlayerData.citizenid}, function(result)
-            if result[1] then
-                cb(result)
-            else
-                cb(nil)
-            end
-        end)
+    if Player and Player.PlayerData and Player.PlayerData.money then
+        Player(src).state:set('money', Player.PlayerData.money, true)
     end
+end)
+
+
+local function updateMoneyState(src, account, amount, isMinus)
+    local Player = RSGCore.Functions.GetPlayer(src)
+    if Player and Player.PlayerData and Player.PlayerData.money then
+        Player(src).state:set('money', Player.PlayerData.money, true)
+        TriggerClientEvent('hud:client:OnMoneyChange', src, account, amount, isMinus)
+    end
+end
+
+
+AddEventHandler('RSGCore:Server:AddMoney', function(src, account, amount)
+    updateMoneyState(src, account, amount, false)
+end)
+
+AddEventHandler('RSGCore:Server:RemoveMoney', function(src, account, amount)
+    updateMoneyState(src, account, amount, true)
+end)
+
+AddEventHandler('RSGCore:Server:SetMoney', function(src, account, amount)
+    updateMoneyState(src, account, amount, false)
+end)
+
+
+RSGCore.Commands.Add('cash', 'Check Cash Balance', {}, false, function(source)
+    local Player = RSGCore.Functions.GetPlayer(source)
+    if Player and Player.PlayerData.money and Player.PlayerData.money.cash ~= nil then
+        TriggerClientEvent('hud:client:ShowAccounts', source, 'cash', Player.PlayerData.money.cash)
+    end
+end)
+
+RSGCore.Commands.Add('bloodmoney', 'Check Bloodmoney Balance', {}, false, function(source)
+    local Player = RSGCore.Functions.GetPlayer(source)
+    if Player and Player.PlayerData.money and Player.PlayerData.money.bloodmoney ~= nil then
+        TriggerClientEvent('hud:client:ShowAccounts', source, 'bloodmoney', Player.PlayerData.money.bloodmoney)
+    end
+end)
+
+
+RSGCore.Functions.CreateCallback('hud:server:getoutlawstatus', function(source, cb)
+    local Player = RSGCore.Functions.GetPlayer(source)
+    if not Player then return cb(nil) end
+    MySQL.query('SELECT outlawstatus FROM players WHERE citizenid = ?', {Player.PlayerData.citizenid}, function(result)
+        cb(result[1] or nil)
+    end)
 end)
